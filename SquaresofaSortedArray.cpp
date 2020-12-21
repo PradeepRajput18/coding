db3f3d59a1d2efa77d9727d8deb128abfa890a91
PradeepRajput18 <51072066+PradeepRajput18@users.noreply.github.com>
2020-12-21 11:37:43 +0530

    Squares of a Sorted Array
    I this question I had done sorted based on two pointers

---
+++ SquaresofaSortedArray.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int sqrt(short m)
+{
+	return m*m;
+}
+
+int main(){
+	short nums[]={-10000,-9999,-7,-5,0,0,10000};
+	short l=0,m=sizeof(nums)/sizeof(nums[0]);
+	m=m-1;
+	int* a=new int[m];
+	short n=m,g=m;
+	while(l<=m){
+		int f=sqrt(nums[l]);
+		int k=sqrt(nums[m]);
+		if(f>=k){
+			a[n]=f;
+			n=n-1;
+			l+=1;
+		}else{
+			a[n]=k;
+			n=n-1;
+			m-=1;
+		}
+	}
+	for(int i=0;i<=g;i++){
+		cout<<a[i]<<"    ";
+	}
+}
