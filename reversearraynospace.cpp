b8eeeb93f9206fb8983a954b71ed96e3d121fee4
PradeepCodeSci <51072066+PradeepRajput18@users.noreply.github.com>
2021-01-03 15:16:58 +0530

    reversearraywithnoextraspace
    reversing array with no extra space used

---
+++ reversearraynospace.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+short swap(short *a,short *b){
+	*a=*a+*b;
+	*b=*a-*b;
+	*a=*a-*b;
+}
+
+int main(){
+	short a[10]={1,2,3,4,5,6,7,8,9,10};
+	short j=sizeof(a)/sizeof(a[0])-1;
+	short i=0;
+	while(i<j){
+		cout<<a[i]<<"value"<<a[j]<<endl;
+		swap(&a[i],&a[j]);
+		i+=1;
+		j-=1;
+	}
+	i=0;
+	while(i<(sizeof(a)/sizeof(a[0]))){
+		cout<<a[i]<<"\t";
+		i++;
+	}
+}
