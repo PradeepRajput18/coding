1cc2b5802591d08ae849bba75b5d81dbae838bb5
PradeepCodeSci <51072066+PradeepRajput18@users.noreply.github.com>
2021-01-06 16:34:53 +0530

    Create findexecutiontime.py

---
+++ findexecutiontime.py
@@ -0,0 +1,17 @@
+
+
+
+find executon time taken for a program in python 
+
+
+import time
+
+start = time.localtime()
+
+logic or code here 
+
+end = time.localtime()
+"""Total execution time in minutes$ """
+print(end.tm_min - start.tm_min)
+"""Total execution time in seconds$ """
+print(end.tm_sec - start.tm_sec)
