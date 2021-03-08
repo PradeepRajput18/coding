ea4b0a6f2fb7a8feb17ca93f3932767b82fb1a11
PradeepCodeSci <51072066+PradeepRajput18@users.noreply.github.com>
2021-03-08 23:31:37 +0530

    Update intersectionoftwolinkedlist1.cpp

--- intersectionoftwolinkedlist1.cpp
+++ intersectionoftwolinkedlist1.cpp
@@ -21,3 +21,53 @@ public:
         
     }
 };
+
+
+// using number difference
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    int lengthA = 0, lengthB = 0;
+    for (ListNode *a = headA; a != NULL; a = a->next) {
+        lengthA++;
+    }
+    for (ListNode *b = headB; b != NULL; b = b->next) {
+        lengthB++;
+    }
+
+    ListNode *a = headA, *b = headB; 
+
+    int difference = abs(lengthA - lengthB);
+    if (lengthA > lengthB) {
+        while (difference--) {
+            a = a->next;
+        }
+    } else if (lengthA < lengthB) {
+        while (difference--) {
+            b = b->next;
+        }
+    }
+
+    for (; a != NULL && b != NULL; a = a->next, b = b->next) {
+        if (a == b) {
+            return a;
+        }
+    }
+
+    return NULL;
+}
+
+
+//  using ternary operator 
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *A=headA,*B=headB;
+        while (headA != headB) { 
+            headA = headA ? headA->next : B;
+            headB = headB ? headB->next : A;
+        }
+        return headA;
+        
+    }
+};
